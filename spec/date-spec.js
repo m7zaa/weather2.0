68d463044e72eae71e2eeb6933f7203f8e0dde4a
micah zaayer and Rachael Bouissey <micah@>
2019-09-05 11:42:41 -0700

    successfully tested syntax.

--- spec/date-spec.js
+++ spec/date-spec.js
@@ -1,8 +1,31 @@
-import { Date } from './../src/date.js';
+// import { Date } from './../src/date.js';
 
 describe ('date syntax', function() {
   it ("it should respond as 'undefined' if the syntax is incorrect", function() {
-    var date1 = new Date('12/26/1981');
-    expect(date1).toEqual("undefined");
+    let date1 = new Date("se").toString().split(" ");
+
+    expect(date1[0]).toEqual("Invalid");
   });
+  // it ("Test for correct input.", function() {
+  //   let date1 = new Date('12/12/69');
+  //   expect(date1).toEqual("undefined");
+  // });
+  // it ("Determine day based on correct input", function() {
+  //   let date1 = new Date('December 17, 1995');
+  //   expect(date1).toEqual("Sun");
+  // });
+  // it ("Program returns day based on correct input", function() {
+  //   let date1 = new Date('December 17, 1995');
+  //   expect(date1).toEqual("Sunday");
+  // });
+
+
+
+
+
+  // it ("it should respond as 'undefined' if the syntax is incorrect", function() {
+  //   let date1 = new Date("september 26, 1986").toString().split(" ");
+  //
+  //   expect(date1[0]).toEqual("Fri");
+  // });
 });
