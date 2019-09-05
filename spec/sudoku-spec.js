5091e1dfd4fd449d22f1e9d2a4580126a7a66d7d
micah zaayer and Rachael Bouissey <rachael@>
2019-09-05 09:52:46 -0700

    add packages and files for project.

--- spec/sudoku-spec.js
+++
@@ -1,8 +0,0 @@
-import { Row } from './../src/sudoku.js';
-
-describe ('Row', function() {
-  it ("it should accept only integers 1-9.", function() {
-    var rowOne = new Row([11]);
-    expect(rowOne.checkRow()).toEqual("This number is not valid.");
-  });
-});
