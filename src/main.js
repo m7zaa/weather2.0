1b77aea3966aaabbac3f64c921ca7cf0e2e4aeb4
Student <example@example.com>
2019-09-10 14:53:19 -0700

    add gif generator.

--- src/main.js
+++ src/main.js
@@ -6,17 +6,17 @@ import { WeatherService } from './weather-service';
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
 
 
-    let weatherService = new WeatherService();  // create instance of WeatherService class
-    let promise = weatherService.getWeatherByCity(city);  // call the instance method and pass in user input
+    let weatherService = new WeatherService();  // create instance of WeatherService class. Change these values to custom your page.
+    let promise = weatherService.getWeatherByCity();  // call the instance method and pass in user input.
 
     promise.then(function(response) {
       const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      console.log(body);
+      //target the img value and display with a img src to display
+      $('.showHumidity').html(`<img src=${body.data.images.original.url}>`);
+
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
--- src/weather-service.js
+++ src/weather-service.js
@@ -1,9 +1,9 @@
 // Back-end
 export class WeatherService {
-  getWeatherByCity(city) {
+  getWeatherByCity() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+      const url = `http://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}`; // watch for back ticks.
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
