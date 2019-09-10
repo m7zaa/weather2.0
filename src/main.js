aef7ad32558a2b38ee023f19eb78c930b8156093
Student <example@example.com>
2019-09-10 15:27:36 -0700

    put together weather api and gif generator into one program.

--- src/main.js
+++ src/main.js
@@ -3,23 +3,48 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { WeatherService } from './weather-service';
+import { GiphyGenerator } from './weather-service';
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+  $('#location').val("");
 
 
-    let weatherService = new WeatherService();  // create instance of WeatherService class. Change these values to custom your page.
-    let promise = weatherService.getWeatherByCity();  // call the instance method and pass in user input.
+    let weatherService = new WeatherService(city);
+    let giphyGenerator = new GiphyGenerator(); // create instance of WeatherService class. Change these values to custom your page.
+    let promise = weatherService.getWeatherByCity(city);  // call the instance method and pass in user input.
+    let promise2 = giphyGenerator.randomGiphy();
 
     promise.then(function(response) {
       const body = JSON.parse(response);
       console.log(body);
       //target the img value and display with a img src to display
-      $('.showHumidity').html(`<img src=${body.data.images.original.url}>`);
+      //weather app display results.------\
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
 
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
+
+
+
+    promise2.then(function(response) {
+      const body = JSON.parse(response);
+      console.log(body);
+      //target the img value and display with a img src to display
+      $('.giphy').html(`<img src=${body.data.images.original.url}>`);
+
+
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+
+
+
+
+
   });
 
 });
--- src/weather-service.js
+++ src/weather-service.js
@@ -1,9 +1,27 @@
 // Back-end
-export class WeatherService {
-  getWeatherByCity() {
+export class GiphyGenerator {
+  randomGiphy() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       const url = `http://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}`; // watch for back ticks.
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+
+export class WeatherService {
+  getWeatherByCity(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OTHER_API_KEY}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
